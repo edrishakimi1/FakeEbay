09c29afcb0b9c4e2b493900701c3b913e3b12a9b
Kärkkäinen Eemi <eemi.karkkainen@aalto.fi>
2021-10-29 13:12:54 +0300

    Improve page styling and add chat

--- src/main/scala/app/MinimalApplication.scala
+++ src/main/scala/app/MinimalApplication.scala
@@ -6,12 +6,29 @@ object MinimalApplication extends cask.MainRoutes{
   @cask.get("/")
   def hello() = {
     html(
-      head(),
+      head(
+        link(
+          rel := "stylesheet",
+          href := "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        )
+      ),
       body(
-        h1("Hello!"),
-        p("World")
+        div(cls := "container")(
+          h1("Scala Chat!"),
+          hr,
+          div(
+            p(b("alice"), " ", "Hello World!"),
+            p(b("bob"), " ", "I am cow, hear me moo"),
+            p(b("charlie"), " ", "I weigh twice as much as you")
+          ),
+          hr,
+          div(
+            input(`type` := "text", placeholder := "User name", width := "20%"),
+            input(`type` := "text", placeholder := "Please write a message!", width := "80%")
+          )
+        )
       )
-    )    
+    )
   }
 
   initialize()
