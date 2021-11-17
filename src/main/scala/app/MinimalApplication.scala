2bf1732206a7584f1f30b325aa31d75a39cff4eb
user1 <mohammad.hakimi@aalto.fi>
2021-11-17 10:32:24 +0200

    redy

--- src/main/scala/app/MinimalApplication.scala
+++ src/main/scala/app/MinimalApplication.scala
@@ -1,88 +1,47 @@
 package app
 import scalatags.Text.all._
+import collection.mutable.Buffer
 
 object MinimalApplication extends cask.MainRoutes{
-  var messages = Vector(
-    ("alice", "Hello World!"),
-    ("bob", "I am cow, hear me moo"),
-    ("charlie", "I weigh twice as you"),
-  )
 
-  @cask.postForm("/")
-  def postHello(name: String, msg: String) = {
-    if (name == "") hello(Some("Name cannot be empty"), Some(name), Some(msg))
-    else if (name.length >= 10) hello(Some("Name cannot be longer than 10 characters"), Some(name), Some(msg))
-    else if (msg == "") hello(Some("Message cannot be empty"), Some(name), Some(msg))
-    else if (msg.length >= 160) hello(Some("Message cannot be longer than 160 characters"), Some(name), Some(msg))
-    else {
-      messages = messages :+ (name -> msg)
-      hello(None, Some(name), None)
-    }
-  }
 
   @cask.get("/")
-  def hello(errorOpt: Option[String] = None,
-            userName: Option[String] = None,
-            msg: Option[String] = None) = {
+  def hello() = {
     html(
-      head(
-        link(
-          rel := "stylesheet",
-          href := "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-       ),
-       script(raw("""
-          function submitForm(){
-            fetch(
-              "/",
-              {
-                method: "POST",
-                body: JSON.stringify({name: nameInput.value, msg: msgInput.value})
-              }
-            ).then(response => response.json())
-             .then(json => {
-              if (json.success) {
-                messageList.innerHTML = json.txt
-                msgInput.value = ""
-                errorDiv.innerText = ""
-              } else {
-                errorDiv.innerText = json.txt
-              }
-            })
-          }
-        """))
-      ),
+      head("NICE TO SEE YOU"),
       body(
-        div(cls := "container")(
-          h1("Scala Chat!"),
-          hr,
-          div(
-            for((name, msg) <- messages)
-            yield p(b(name), " ", msg)
-          ),
-          hr,
-          div(id := "errorDiv", color.red),
-          form(onsubmit := "submitForm(); return false")(
-            input(
-              `type` := "text",
-              id := "nameInput",
-              placeholder := "User name",
-              width := "20%",
-              userName.map(value := _)
-            ),
-            input(
-              `type` := "text",
-              id := "msgInput",
-              placeholder := "Please write a message!",
-              width := "60%",
-              msg.map(value := _)
-            ),
-            input(`type` := "submit", width := "20%")
-          )
-        )
+       h1("Welcome to our JEWELRY SHOP"),
+      p("We sell  jewelry at a good price and our prices can be found when you inject / Prices")
+
+    )
+      , form(action:= "/", method := "pos")(
+        select(id:= "korut-valitse", name:="korut"),
+          option("valitse ", value:="")),
+      option("please choose")
       )
-    ).render
+
+
   }
 
-  initialize()
+
+
+  @cask.get("/Prices")
+  def korujentiedot() = {
+    val tiedot = Buffer("gold jewelry 800 Euro","silver jewelry 200 Euro","diamond jewelry 10000 Euro" )
+    val listaa = for(korut <- tiedot) yield li(korut)
+    html(
+      head(),
+      body(
+       //div(cls := "container")(
+          ul(
+            listaa.toSeq
+
+    )
+      )
+    )
+  }
+
+ initialize()
+
 }
 
